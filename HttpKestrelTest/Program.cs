6d9648ab2de0ede9bf57bf6e9a69ffc91ff9290f
Thad House <thadhouse1@gmail.com>
2017-12-10 22:18:32 -0800

    Initial commit

---
+++ HttpKestrelTest/Program.cs
@@ -0,0 +1,122 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Transport.Abstractions.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace SampleApp
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            var logger = loggerFactory.CreateLogger("Default");
+
+            app.Run(async context =>
+            {
+                var connectionFeature = context.Connection;
+                string toWrite = ($"Peer: {connectionFeature.RemoteIpAddress?.ToString()}:{connectionFeature.RemotePort}"
+                    + $"{Environment.NewLine}"
+                    + $"Sock: {connectionFeature.LocalIpAddress?.ToString()}:{connectionFeature.LocalPort}");
+
+                logger.LogDebug(toWrite);
+
+                string method = context.Request.Method;
+
+                string routePath = context.Request.GetEncodedPathAndQuery();
+
+                if (method == "GET")
+                {
+                    // Perform anything with a GET here
+                    var response = "[1,2,3]";
+                    context.Response.ContentLength = response.Length;
+                    context.Response.ContentType = "application/json";
+                    await context.Response.WriteAsync(response);
+                    ;
+                }
+                else if (method == "POST")
+                {
+                    using (StreamReader reader = new StreamReader(context.Request.Body))
+                    {
+                        string request = await reader.ReadToEndAsync();
+                        context.Response.StatusCode = 404;
+                        
+                        // Perform anything with a POST here
+                        ;
+                    }
+                }
+
+                Console.WriteLine(context.Request.Method);
+
+                Console.WriteLine(context.Request.GetTypedHeaders());
+                Console.WriteLine(context.Request.ContentType);
+                Console.WriteLine(context.Request.Body);
+
+
+            });
+        }
+
+        public static void Main(string[] args)
+        {
+            TaskScheduler.UnobservedTaskException += (sender, e) =>
+            {
+                Console.WriteLine("Unobserved exception: {0}", e.Exception);
+            };
+
+            var configuration = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build();
+
+            if (!ushort.TryParse(configuration["BASE_PORT"], NumberStyles.None, CultureInfo.InvariantCulture, out var basePort))
+            {
+                basePort = 5000;
+            }
+
+            var host = new WebHostBuilder()
+                .ConfigureLogging((_, factory) =>
+                {
+                    factory.AddConsole();
+                    
+                    
+                })
+                .UseKestrel(options =>
+                {
+                    // Run callbacks on the transport thread
+                    options.ApplicationSchedulingMode = SchedulingMode.Inline;
+
+                    options.Listen(IPAddress.Loopback, basePort, listenOptions =>
+                    {
+                        // Uncomment the following to enable Nagle's algorithm for this endpoint.
+                        //listenOptions.NoDelay = false;
+
+                        listenOptions.UseConnectionLogging();
+                    });
+
+                    options.UseSystemd();
+
+                    // The following section should be used to demo sockets
+                    //options.ListenUnixSocket("/tmp/kestrel-test.sock");
+                })
+                .UseLibuv(options =>
+                {
+                    // Uncomment the following line to change the default number of libuv threads for all endpoints.
+                    // options.ThreadCount = 4;
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
